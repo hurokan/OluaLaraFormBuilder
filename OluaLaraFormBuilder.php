abef8b87dec69274766c12c0ea4d78c09dd83ee4
Hasan Uddin Rokan <hurokan91@gmail.com>
2023-12-02 10:06:10 +0600

    files

---
+++ OluaLaraFormBuilder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rokan\OluaLaraFormBuilder;
+
+class OluaLaraFormBuilder
+{
+    // Build your next great package.
+    public static function testReturn(){
+        return "this is calling";
+    }
+}
---
+++ src/SampleClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rokan\OluaLaraFormBuilder;
+
+class SampleClass
+{
+    public function sampleMethod()
+    {
+        return 'This is a method from your package!';
+    }
+}
